377ebfd3f315c0ef07c8a451ef4fe5f4b6b161df
Susanamanrique11 <133910057+Susanamanrique11@users.noreply.github.com>
2023-06-02 10:07:24 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import Header from './shared/header'
 import Footer from './shared/footer'
 import Inicio from './paginas/inicio'
-import AppStyle from './App.module.css'
+import AppStyle from './app.module.css'
 
 
 function App() {
