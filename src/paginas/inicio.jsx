108cb2bf3dbd2446d84f423f2bc6c2a9b6087f48
Susanamanrique11 <susanamanrique4@gmail.com>
2023-06-01 20:43:34 -0500

    Actualizacion 6

--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -1,4 +1,5 @@
 import Slider from "./components/slider/Slider"
+/*import datosproductos, {datosproductos} from "./datosproductos";*/
 
 const Inicio = function () {
 
@@ -8,11 +9,16 @@ const Inicio = function () {
         'https://scontent.fbog3-2.fna.fbcdn.net/v/t39.30808-6/350255617_1001715284601939_5231825082315580286_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=qZm5e23UqIgAX8UwxgL&_nc_ht=scontent.fbog3-2.fna&oh=00_AfAaLlOaFnbgA2QHRp-PEcGSTIf8u2RsI6jNvutFIWw-cQ&oe=647DC8E8',
     ];
 
+    /*const elemento1 = document.getElementById ("1");*/
+
     return(<>
     <section className="promociones"> {/*Carrusel con links*/}
         <Slider imagenes={mockImagenes} />
     </section>
     <section className="productos_destacados">  {/*Flexbox con links*/}
+        <section>
+           
+        </section>
     </section>
     <section className="ir_catalogo"> {/*Botón*/}
     </section>
