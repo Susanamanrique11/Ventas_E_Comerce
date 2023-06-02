d767418416c0494878b95281acaa800248bacec0
Susanamanrique11 <susanamanrique4@gmail.com>
2023-06-02 00:33:54 -0500

    Cambios 7

--- src/paginas/carrito.jsx
+++ src/paginas/carrito.jsx
@@ -1,13 +1,36 @@
-import Header from '../shared/header'
-import Footer from '../shared/footer'
+import React, { useState } from "react";
 
-const Carrito = function () {
-    return (<>
-        <Header></Header>
-        <section className="productos_comprados"></section>
-        <section className="resumen_pedido"></section>
-        <Footer></Footer>
-    </>)
-}
+const Carrito = () => {
+  const [productos, setProductos] = useState([]);
 
-export default Carrito
+  const agregarProducto = (producto) => {
+    setProductos([...productos, producto]);
+  };
+
+  const eliminarProducto = (producto) => {
+    const nuevosProductos = productos.filter((p) => p.id !== producto.id);
+    setProductos(nuevosProductos);
+  };
+
+  return (
+    <div>
+      <h2>Carrito de compras</h2>
+      {productos.length === 0 ? (
+        <p>No hay productos en el carrito</p>
+      ) : (
+        <ul>
+          {productos.map((producto) => (
+            <li key={producto.id}>
+              {producto.name} - ${producto.price}
+              <button onClick={() => eliminarProducto(producto)}>
+                Retirar del carrito
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Carrito;
--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import Slider from "./components/slider/Slider"
-/*import datosproductos, {datosproductos} from "./datosproductos";*/
+import datosproductos from "./datosproductos"
+import InicioStyle from "../estilos/inicio.module.css"
 
 const Inicio = function () {
 
@@ -9,7 +11,11 @@ const Inicio = function () {
         'https://scontent.fbog3-2.fna.fbcdn.net/v/t39.30808-6/350255617_1001715284601939_5231825082315580286_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=qZm5e23UqIgAX8UwxgL&_nc_ht=scontent.fbog3-2.fna&oh=00_AfAaLlOaFnbgA2QHRp-PEcGSTIf8u2RsI6jNvutFIWw-cQ&oe=647DC8E8',
     ];
 
-    /*const elemento1 = document.getElementById ("1");*/
+    const elemento1 = datosproductos[0];
+    const elemento2 = datosproductos[5];
+    const elemento3 = datosproductos[9];
+    const elemento4 = datosproductos[14];
+
 
     return(<>
     <section className="promociones"> {/*Carrusel con links*/}
@@ -17,10 +23,44 @@ const Inicio = function () {
     </section>
     <section className="productos_destacados">  {/*Flexbox con links*/}
         <section>
-           
+            <ul>
+                <li>{elemento1.name}</li>
+                <li>{elemento1.generalDescription}</li>
+                <li><img src={elemento1.image} alt="" /></li>
+                <li>{elemento1.description}</li>
+                <li>{elemento1.price}</li>
+                <li>{elemento1.attributes.size}</li>
+                <li>{elemento1.attributes.nutritionInfo}</li>
+                <li>{elemento1.attributes.flavors}</li>
+            </ul>
+            <ul>
+                <li>{elemento2.name}</li>
+                <li>{elemento2.generalDescription}</li>
+                <li><img src={elemento2.image} alt="" /></li>
+                <li>{elemento2.description}</li>
+                <li>{elemento2.price}</li>
+                <li>{elemento2.attributes.size}</li>
+            </ul>
+            <ul>
+                <li>{elemento3.name}</li>
+                <li>{elemento3.generalDescription}</li>
+                <li><img src={elemento3.image} alt="" /></li>
+                <li>{elemento3.description}</li>
+                <li>{elemento3.price}</li>
+            </ul>
+            <ul>
+                <li>{elemento4.name}</li>
+                <li>{elemento4.generalDescription}</li>
+                <li><img src={elemento4.image} alt="" /></li>
+                <li>{elemento4.description}</li>
+                <li>{elemento4.price}</li>
+                <li>{elemento4.attributes.colors}</li>
+                <li>{elemento4.attributes.characteristics}</li>
+            </ul>
         </section>
     </section>
     <section className="ir_catalogo"> {/*Botón*/}
+        <button className={InicioStyle.bcatalogo}> <Link to = "/catalogo"/></button>
     </section>
     </>)
 }
