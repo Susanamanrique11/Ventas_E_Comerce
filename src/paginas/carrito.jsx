3209818488ff0b86e9447e4476a5d7714359b40d
Susanamanrique11 <133910057+Susanamanrique11@users.noreply.github.com>
2023-06-04 08:37:39 -0500

    Update carrito.jsx

--- src/paginas/carrito.jsx
+++ src/paginas/carrito.jsx
@@ -1,36 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../shared/header";
+import Footer from "../shared/footer";
+import datosproductos from "./datosproductos";
+import CarritoStyle from '../estilos/carrito.module.css'
 
 const Carrito = () => {
-  const [productos, setProductos] = useState([]);
+  const [carrito, setCarrito] = useState([]);
+  const { productId } = useParams();
+  console.log(productId);
 
-  const agregarProducto = (producto) => {
-    setProductos([...productos, producto]);
+  useEffect(() => {
+    // Verificar que el productId sea un número válido antes de agregarlo al carrito
+    if (!isNaN(productId) && productId > 0) {
+      const producto = datosproductos.find((item) => item.id === parseInt(productId));
+      if (producto) {
+        addToCart(producto); // Agregar el producto al carrito
+      }
+    }
+  }, [productId]);
+
+  const addToCart = (producto) => {
+    setCarrito([...carrito, producto]);
   };
 
-  const eliminarProducto = (producto) => {
-    const nuevosProductos = productos.filter((p) => p.id !== producto.id);
-    setProductos(nuevosProductos);
+  const removeFromCart = (producto) => {
+    const updatedCart = carrito.filter((item) => item.id !== producto.id);
+    setCarrito(updatedCart);
+  };
+
+  const renderCartItems = () => {
+    if (carrito.length === 0) {
+      return <p>No hay productos en el carrito</p>;
+    }
+
+    return carrito.map((producto) => (
+      <div key={producto.id} className={CarritoStyle.carrito}>
+        <img src={producto.image} alt={producto.name}/>
+        <p>{producto.name}</p>
+        <p>Precio: ${producto.price}</p>
+        <button onClick={() => removeFromCart(producto)}>Eliminar</button>
+      </div>
+    ));
   };
 
   return (
-    <div>
-      <h2>Carrito de compras</h2>
-      {productos.length === 0 ? (
-        <p>No hay productos en el carrito</p>
-      ) : (
-        <ul>
-          {productos.map((producto) => (
-            <li key={producto.id}>
-              {producto.name} - ${producto.price}
-              <button onClick={() => eliminarProducto(producto)}>
-                Retirar del carrito
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className={CarritoStyle.general}>
+      <Header />
+      <h2>Carrito de Compras</h2>
+      {renderCartItems()}
+      <Footer />
     </div>
   );
 };
 
-export default Carrito;
+export default Carrito;
